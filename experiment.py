374ddad8fe15b58fff8864a175a4e1ded82e36c6
bryant <bryant0918@gmail.com>
2024-05-21 17:02:29 -0600

    added experminet file to git

---
+++ experiment.py
@@ -0,0 +1,174 @@
+from model import FastSpeech2_Pros
+import torch
+import yaml
+import numpy as np
+from synthesize import preprocess_english
+from transformer.Models import BaseProsodyPredictor, ProsodyPredictor, ProsodyExtractor
+import pickle
+from audio.tools import get_mel_from_wav
+import audio as Audio
+import librosa
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print("Device:", device)
+
+preprocess_config = "config/LJSpeech/preprocess.yaml"
+model_config = "config/LJSpeech/model.yaml"
+
+# Read Config
+preprocess_config = yaml.load(open(preprocess_config, "r"), Loader=yaml.FullLoader)
+model_config = yaml.load(open(model_config, "r"), Loader=yaml.FullLoader)
+
+"""Test Prosody Extractor"""
+test_extractor = True
+if test_extractor:
+    # Create the model
+    model = ProsodyExtractor(256, 1, 4, 8).to(device)
+    # Print the model summary
+    print(model)
+    config = preprocess_config
+
+    STFT = Audio.stft.TacotronSTFT(
+        config["preprocessing"]["stft"]["filter_length"],
+        config["preprocessing"]["stft"]["hop_length"],
+        config["preprocessing"]["stft"]["win_length"],
+        config["preprocessing"]["mel"]["n_mel_channels"],
+        config["preprocessing"]["audio"]["sampling_rate"],
+        config["preprocessing"]["mel"]["mel_fmin"],
+        config["preprocessing"]["mel"]["mel_fmax"],
+    )
+
+    wav_path = "/Users/bryantmcarthur/Documents/Ditto/experiment/tony.flac"
+    sampling_rate = config["preprocessing"]["audio"]["sampling_rate"]
+
+    # Read and trim wav files
+    wav, _ = librosa.load(wav_path)
+    wav = torch.from_numpy(wav.astype(np.float32))
+
+    melspec, energy = get_mel_from_wav(wav, STFT)
+
+    melspec = torch.from_numpy(melspec).to(device)
+
+    print("Mel shape: ", melspec.size())
+
+    # TODO: Match Dimensions :)
+    e = model(melspec)
+    print("e shape: ", e.size())
+
+
+"""Test Prosody Predictor"""
+test_predictor = False
+if test_predictor:
+    model = FastSpeech2_Pros(preprocess_config, model_config).to(device)
+
+    text = "Hello, how are you doing today?"
+    texts = np.array([preprocess_english(text, preprocess_config)])
+
+    print("Texts shape: ", texts.shape)
+
+    ids = raw_texts = [text[:100]]
+    speakers = np.array([0])
+    text_lens = np.array([len(texts[0])])
+
+    speakers = torch.from_numpy(speakers).long().to(device)
+    texts = torch.from_numpy(texts).long().to(device)
+    src_lens = torch.from_numpy(text_lens).to(device)
+
+    max_src_lens = max(text_lens)
+    print(max_src_lens)
+
+    batch = (speakers, texts, src_lens, max_src_lens)
+
+    with torch.no_grad():
+        # Forward
+        output = model(*batch)[0]
+        print("Output shape: ", output.size())
+
+    h_si = torch.rand([1, 19, 256], device=device)
+    print("h_si shape: ", h_si.size())
+
+    # Get Speaker Embedding created by speaker embedding repo
+    # style_wav = "/Users/bryantmcarthur/Documents/Ditto/experiment/tony.flac"
+    embedding_path = "/Users/bryantmcarthur/Documents/Ditto/SpeakerEncoder/outputs/out.pkl_emb.pkl"
+
+    with open(embedding_path, 'rb') as f:
+        emb_dict = pickle.load(f)
+
+    embedding = torch.from_numpy(emb_dict["default"]).to(device)
+    print("Embedding shape: ", embedding.size())   # TODO: Embedding dim needs to match output of encoder (rn 128 vs 256)
+
+    embedding = torch.cat((embedding, embedding))
+    print("Embedding shape: ", embedding.size())
+
+    # Adding new dimensions to tensor_2
+    embedding = embedding.unsqueeze(0).unsqueeze(0).expand(-1, 19, -1)
+
+    h_sd = h_si + embedding
+
+    print("h_sd shape: ", h_sd.size())
+
+    # Create the model
+    model = ProsodyPredictor(256, 1, 4, 8).to(device)
+    # Print the model summary
+    print(model)
+
+    e = model(h_sd, h_si)
+
+    pi, mu, sigma = e
+    print("pi shape: ", pi.size())
+    print("mu shape: ", mu.size())
+    print("sigma shape: ", sigma.size())
+
+# def plot_data(x, y):
+#     plt.hist2d(x, y, bins=35)
+#     plt.xlim(-8, 8)
+#     plt.ylim(-1, 1)
+#     plt.axis('off')
+#
+#
+# x, y = gen_data()
+# x = torch.Tensor(x)
+# y = torch.Tensor(y)
+#
+# print(x.size())
+# print(y.size())
+#
+# n_iterations = 10
+#
+# # Create the model
+# model = BaseProsodyPredictor(1, 1, 4, 8)
+# # Print the model summary
+# print(model)
+#
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, n_iterations)
+#
+# for i in range(n_iterations):
+#     optimizer.zero_grad()
+#     if i == 0:
+#         e = model(x)
+#     else:
+#         e = model(x, e)
+#     loss = model.phone_loss(e, y).mean()
+#     loss.backward()
+#     optimizer.step()
+#     scheduler.step()
+#
+# print("Loss: ", loss.item)
+#
+# with torch.no_grad():
+#     y_hat = model.sample(x)
+#
+# plt.figure(figsize=(8, 3))
+# plt.subplot(1, 2, 1)
+# plot_data(x[:, 0].numpy(), y[:, 0].numpy())
+# plt.title("Observed data")
+# plt.subplot(1, 2, 2)
+# plot_data(x[:, 0].numpy(), y_hat[:, 0].numpy())
+# plt.title("Sampled data")
+# plt.show()

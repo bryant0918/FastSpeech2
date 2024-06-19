fixed floating point math precision bug

dataset.py
@@ -8,7 +8,7 @@ import torch
 from torch.utils.data import Dataset
 
 from text import text_to_sequence
-from utils.tools import pad_1D, pad_2D, pad_inhomogeneous_2D
+from utils.tools import pad_1D, pad_2D, pad_inhomogeneous_2D, flip_mapping
 
 
 class TrainDataset(Dataset):
@@ -143,10 +143,15 @@ class TrainDataset(Dataset):
         translations = pad_1D(translations)
         alignments = pad_inhomogeneous_2D(alignments)
 
+        # Debugging
+        reverse_alignments = flip_mapping(torch.from_numpy(alignments).int())
         print("Shape of alignments", np.shape(alignments))
         print("Shape of texts", np.shape(texts))
         print("Shape of translations", np.shape(translations))
-        print()
+        print("Shape of reverse_alignments", reverse_alignments.shape)
+        if np.shape(alignments)[1] != np.shape(translations)[1] or reverse_alignments.shape[1] != np.shape(texts)[1]:
+            raise ValueError("Alignments and Texts must have the same length")
+            
 
         return (ids, raw_texts, raw_translations, speakers, texts, text_lens, max(text_lens), mels, mel_lens,
                 max(mel_lens), translations, translation_lens, max(translation_lens), speaker_embeddings, alignments, pitches, energies,

experiment.py
@@ -451,7 +451,7 @@ if test_textgrid:
             durations.append(int(np.round(e * 22050 / 256) - np.round(s * 22050 / 256)))
 
         print("Phones: ", all_phones)
-        print(len(all_phones), word_idx, num_words)
+        print(len(all_phones), word_idx, num_words, end_idx)
         print('length of durations pre slice', len(durations))
 
         # Trim tailing silences
@@ -471,8 +471,8 @@ if test_textgrid:
     # tg_path = "preprocessed_data/LJSpeech/TextGrid/LJSpeech/LJ032-0007.TextGrid"
     # tg_path = "preprocessed_data/LJSpeech/TextGrid/LJSpeech/LJ036-0179.TextGrid"
     # tg_path = "preprocessed_data/LJSpeech/TextGrid/LJSpeech/LJ030-0041.TextGrid"
-    # tg_path = "preprocessed_data/LJSpeech/TextGrid/LJSpeech/LJ050-0116.TextGrid"
-    tg_path = "preprocessed_data/LJSpeech/TextGrid/LJSpeech/LJ015-0071.TextGrid"
+    # tg_path = "preprocessed_data/LJSpeech/TextGrid/LJSpeech/LJ002-0298.TextGrid"
+    tg_path = "preprocessed_data/LJSpeech/TextGrid/LJSpeech/LJ048-0270.TextGrid"
     textgrid = tgt.io.read_textgrid(tg_path)
 
     phones, durations, start, end = get_alignment(textgrid)
@@ -546,7 +546,7 @@ if test_word_alignment:
     print(len(phone_alignments))
 
 """Test Sentence Aligner"""
-test_aligner = True
+test_aligner = False
 if test_aligner:
     from simalign import SentenceAligner
     # making an instance of our model.
@@ -763,8 +763,8 @@ if test_phone_alignment:
         """
         phone_alignments = {}
 
-        print("src_phones", src_phones)
-        print("tgt_phones", tgt_phones)
+        print("src_phones", len(src_phones), src_phones)
+        print("tgt_phones", len(tgt_phones), tgt_phones)
         # print("word_alignments", word_alignments)
         
         src_phone_cumsums = np.cumsum([len(src_phone) for src_phone in src_phones])
@@ -817,11 +817,16 @@ if test_phone_alignment:
 
             # print("len of tgt_wrd_phones", len(tgt_word_phones))
             while tgt_phone < len(tgt_word_phones):
+                if flat_src_phones_idx == len(flat_src_phones):
+                    flat_src_phones_idx -= 1
+                    current_src_phone -= 1
+                
                 if (1-phone_accumulations) > phone_weight:   # Use all of the phone_weight left
                     phone_accumulations += phone_weight
                     if current_src_phone not in phone_alignment:
                         phone_alignment.append(current_src_phone)
                         flat_phone_alignment.append(flat_src_phones_idx)
+                        
                     # Reset
                     phone_weight = len(src_word_phones) / len(tgt_word_phones)
                     tgt_phone += 1
@@ -881,7 +886,7 @@ if test_phone_alignment:
 
         return flat_phone_alignments
 
-    pth = "preprocessed_data/LJSpeech/alignments/word/LJSpeech-word_alignment-LJ015-0071.npy"
+    pth = "preprocessed_data/LJSpeech/alignments/word/LJSpeech-word_alignment-LJ048-0270.npy"
     # Loading the dictionary
     # with open(pth, 'rb') as f:
     #     word_alignments = pickle.load(f)
@@ -897,7 +902,7 @@ if test_phone_alignment:
     new_punc = "¡!\"#$%&'()*+,-./:;<=>¿?@[\]^_`{|}~"
 
     # text = "que tienen la responsabilidad principal de suministrar informacion sobre amenazas potenciales,"
-    pth = "raw_data/LJSpeech/LJSpeech/LJ015-0071_tgt.lab"
+    pth = "raw_data/LJSpeech/LJSpeech/LJ048-0270_tgt.lab"
     with open(pth, 'r') as f:
         text = f.read()
     print("Tgt text: ", text)

model/modules.py
@@ -108,15 +108,11 @@ class VarianceAdaptor(nn.Module):
                 e_control=1.0, d_control=1.0):
 
         log_duration_prediction = self.duration_predictor(x, src_mask)
+        
         if self.pitch_feature_level == "phoneme_level":
             pitch_prediction, pitch_embedding = self.get_pitch_embedding(
                 x, pitch_target, src_mask, p_control
             )
-            print()
-            print("pitch_target: ", pitch_target.shape)
-            print("pitch_embedding: ", pitch_embedding.shape)
-            print("src_mask: ", src_mask.shape)
-            print("x: ", x.shape)
             x = x + pitch_embedding    # (durations and targets mismatch)
         if self.energy_feature_level == "phoneme_level":
             energy_prediction, energy_embedding = self.get_energy_embedding(

train.py
@@ -192,6 +192,9 @@ def main(args, configs):
                                 #  None, None, None, batch[13], alignments, output_tgt[2], output_tgt[3], output_tgt[4], None, 1.0, 1.0, 1.0)
                 
                 max_mel_len = np.int64(max(output_tgt[9]).cpu().numpy())
+                
+                # output_tgt[4] is log_d_predictions
+                d_src = torch.clamp(torch.round(torch.exp(output_tgt[4] - 1)).long(), min=0)
 
                 print()
                 print(f"texts shape: {batch[4].shape}")
@@ -205,6 +208,7 @@ def main(args, configs):
                 print(f"p_targets shape: {output_tgt[2].shape}")  # Pitches energies and durations should be same size as texts
                 print(f"e_targets shape: {output_tgt[3].shape}")
                 print(f"d_targets shape: {output_tgt[4].shape}")
+                print(f"d_src shape: {d_src.shape}")
                 # print()
                 # print("Pitches: ", batch[15])
 
@@ -212,16 +216,20 @@ def main(args, configs):
                 realigned_p = realign_p_e_d(alignments, output_tgt[2])
                 realigned_e = realign_p_e_d(alignments, output_tgt[3])
                 realigned_d = realign_p_e_d(alignments, output_tgt[4])
+                realigned_d_src = realign_p_e_d(alignments, d_src)
+
                 print("Realigned pitches: ", realigned_p.shape)
                 print("Realigned energies: ", realigned_e.shape)
                 print("Realigned durations: ", realigned_d.shape)
+                print("Realigned durations src: ", realigned_d_src.shape)
                 print()
 
                 # # Forward pass: Tgt to Src (so tgt is now src and src is now tgt)
+                print("\nFORWARD PASS: TGT to SRC")
                 output_src = model(texts=batch[4], src_lens=batch[5], max_src_len=batch[6],
                                    mels=output_tgt[1], mel_lens=output_tgt[9], max_mel_len=max_mel_len,
                                    speaker_embs=batch[13], alignments=alignments, p_targets=realigned_p, 
-                                   e_targets=realigned_e, d_targets=realigned_d, d_src=output_tgt[4])
+                                   e_targets=realigned_e, d_targets=realigned_d_src, d_src=d_src)
                 
                 # # Calculate loss for Tgt to Src
                 print("\nCalculating Loss for TGT to SRC")
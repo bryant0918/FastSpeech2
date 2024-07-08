525df756903ce04ef48964d163bcf30040a950f8
Bryant McArthur <91379982+bryant0918@users.noreply.github.com>
2024-07-08 12:44:41 -0600

    Update loss.py

--- model/loss.py
+++ model/loss.py
@@ -107,17 +107,15 @@ class FastSpeech2Loss(nn.Module):
             mel_targets = mel_targets.masked_select(mel_masks.unsqueeze(-1))
 
             # Penalize later parts of sequence more:
-            # position_weights = torch.linspace(0.25, 1, mel_masks.shape[1]).unsqueeze(-1).to(device).requires_grad_(False)
-            # position_weights /= torch.sum(position_weights)
+            position_weights = torch.linspace(0.25, 1, mel_masks.shape[1]).unsqueeze(-1).to(device).requires_grad_(False)
+            position_weights /= torch.sum(position_weights)
 
             mel_loss = self.mae_loss(mel_predictions, mel_targets)
             postnet_mel_loss = self.mae_loss(postnet_mel_predictions, mel_targets)
 
-            print("mel_loss: ", mel_loss)
-
-            # If multiply by position weights:
-            # mel_loss = mel_loss * position_weights
-            # postnet_mel_loss = postnet_mel_loss * position_weights
+            # Multiply by position weights and sum to get scalar loss
+            mel_loss = torch.sum(mel_loss * position_weights)
+            postnet_mel_loss = torch.sum(postnet_mel_loss * position_weights)
 
             # TODO: Figure out best beta value
             beta = .3
